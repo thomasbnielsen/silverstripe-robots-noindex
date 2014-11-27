da259e0e07d2e4998d70e63867ade761ea7cad20
Thomas B. Nielsen <mail@nobrainer.dk>
2014-11-27 09:44:03 +0100

    Extension now set index or not based on director isDev or isTest

--- code/RobotsNoindexCMSExtensions.php
+++ code/RobotsNoindexCMSExtensions.php
@@ -1,28 +1,12 @@
 <?php
- 
+
 class RobotsNoindexCMSExtensions extends DataExtension {
-	 
-    public function updateCMSFields(FieldList $fields) {
-	
-		if(Director::isDev()) {
-			$fields->addFieldToTab("Root.Main", new LiteralField("IsDevWarning", 
-				"<p class=\"message warning\">" . _t("RobotsNoindex.IS_DEV_WARNING", 
-				"Warning: This website is running in DEV mode!")
-				. "</p>"), "Title");
-		}
-					
-		if(SiteConfig::current_site_config()->IndexCode == "noindex") {
-			$fields->addFieldToTab("Root.Main", new LiteralField("NoindexWarning", 
-				"<p class=\"message warning\">" . _t("RobotsNoindex.NO_INDEX_WARNING", 
-				"Warning: This website is set to NOINDEX and will not be indexed in searchengines!")
-				. "</p>"), "Title");
-		}		
-		
-	}
-	
+
 	public function MetaTags(&$tags) {
-		$tags .= '<meta name="robots" content="'.SiteConfig::current_site_config()->IndexCode.'" />';
+		if (Director::isDev() OR Director::isTest()) {
+			$tags .= '<meta name="robots" content="noindex, nofollow" />';
+		}
 		return $tags;
 	}
-	
+
 }
